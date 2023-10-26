8ad19bdf14b29b3317bf648f341205f85faa2cef
daryle <daryle.tan@gmail.com>
2023-10-25 19:44:05 -0500

    added image upload and admin user,product,order features

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
 import asyncHandler from "../middleware/asyncHandler.js"
-import User from "../models/userModel.js"
 import generateToken from "../utils/generateToken.js"
+import User from "../models/userModel.js"
 
-// @desc Auth user & get token
-// @route POST /api/login
-// @access Public
+// @desc    Auth user & get token
+// @route   POST /api/users/auth
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
@@ -13,7 +13,7 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id)
 
-    res.status(200).json({
+    res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -25,9 +25,9 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc Register user
-// @route POST /api/users
-// @access Public
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
@@ -59,26 +59,25 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc Logout user / clear cookie
-// @route POST /api/logout
-// @access Private
-const logoutUser = asyncHandler(async (req, res) => {
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
   res.cookie("jwt", "", {
     httpOnly: true,
     expires: new Date(0),
   })
+  res.status(200).json({ message: "Logged out successfully" })
+}
 
-  res.status(200).json({ message: "Logged out successful" })
-})
-
-// @desc Get user profile
-// @route GET /api/users/profile
-// @access Public
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
   if (user) {
-    res.status(200).json({
+    res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -90,9 +89,9 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc Update user profile
-// @route PUT /api/users/profile
-// @access Private
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
@@ -106,9 +105,9 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 
     const updatedUser = await user.save()
 
-    res.status(200).json({
+    res.json({
       _id: updatedUser._id,
-      name: updateUser.name,
+      name: updatedUser.name,
       email: updatedUser.email,
       isAdmin: updatedUser.isAdmin,
     })
@@ -118,32 +117,69 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc Get users
-// @route GET /api/users
-// @access Private/Admin
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("get users")
+  const users = await User.find({})
+  res.json(users)
 })
 
-// @desc Get user by ID
-// @route GET /api/users/:id
-// @access Private/Admin
-const getUserByID = asyncHandler(async (req, res) => {
-  res.send("get user by id")
-})
-
-// @desc Delete users
-// @route DELETE /api/users/:id
-// @access Private/Admin
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("delete users")
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400)
+      throw new Error("Can not delete admin user")
+    }
+    await User.deleteOne({ _id: user._id })
+    res.json({ message: "User removed" })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
 })
 
-// @desc Update users
-// @route PUT /api/users/:id
-// @access Private/Admin
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password")
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("update user")
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = Boolean(req.body.isAdmin)
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
 })
 
 export {
@@ -151,9 +187,9 @@ export {
   registerUser,
   logoutUser,
   getUserProfile,
-  getUserByID,
-  getUsers,
   updateUserProfile,
+  getUsers,
   deleteUser,
+  getUserById,
   updateUser,
 }
